0cfe0bf2530640f9c842c68de8a31afb266650d9
Anonymous <anonymous@example.com>
2016-02-20 19:44:21 +0100

    Relationship belongs to user and follower, which is a User. A relationship has to have a user and a follower.

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,6 @@
 class Relationship < ApplicationRecord
+  belongs_to :user
+  belongs_to :follower, class_name: User
+
+  validates :user, :follower, presence: true
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryGirl.build :relationship }
+  
+  describe "validations" do
+    it { is_expected.to be_valid }
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to validate_presence_of(:follower) }
+  end
+
 end
