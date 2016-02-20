77fc42e463718c06c0fa727c5de3691cc5d83fbc
Anonymous <anonymous@example.com>
2016-02-20 19:43:19 +0100

    Schema with migrated relationships.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220131133) do
+ActiveRecord::Schema.define(version: 20160220183205) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "follower_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
