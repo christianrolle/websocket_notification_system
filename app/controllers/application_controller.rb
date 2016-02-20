9edf6f1870b1169de4bb2e94bb5eb07a594e8d51
Anonymous <anonymous@example.com>
2016-02-20 16:26:40 +0100

    Adds blackbox authentication to all controllers. This is used for distinguishing users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+    
+  def current_user
+    @current_user
+  end
+
+  private
+  
+  def authenticate_user!
+    authenticate_user(cookies.signed[:user_id]) || redirect_to(new_session_url)
+  end
+    
+  def authenticate_user user_id
+    @current_user = User.find_by id: user_id
+    cookies.signed[:user_id] ||= user_id if @current_user
+  end
 end
