7ad9474875226c7a94e2d6f06317cdd275329302
Anonymous <anonymous@example.com>
2016-02-23 08:49:45 +0100

    Adds index to relationship model.

--- db/migrate/20160220183205_create_relationships.rb
+++ db/migrate/20160220183205_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :relationships do |t|
-      t.integer :user_id
-      t.integer :follower_id
+      t.belongs_to :user, index: true
+      t.belongs_to :follower, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220183205) do
+ActiveRecord::Schema.define(version: 20160220210212) do
+
+  create_table "notifications", force: :cascade do |t|
+    t.text     "topic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "follower_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
+  create_table "user_notifications", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "notification_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["notification_id"], name: "index_user_notifications_on_notification_id"
+    t.index ["user_id"], name: "index_user_notifications_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
