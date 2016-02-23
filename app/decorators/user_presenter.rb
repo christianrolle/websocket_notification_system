3017bb9b9379081b3d7bc7961f5222e3b2de0d60
Anonymous <anonymous@example.com>
2016-02-23 11:17:54 +0100

    Extracts text for following into separate method and fixes wrong id for leading user.

--- app/decorators/user_presenter.rb
+++ app/decorators/user_presenter.rb
@@ -2,9 +2,13 @@ class UserPresenter < Presenter
   alias_method :user, :model
 
   def link_to_relationship other_user
-    return 'Following' if user.led_by? other_user
-    path = h.relationships_path(relationship: { user_id: user.id }, 
+    return following_text if user.led_by? other_user
+    path = h.relationships_path(relationship: { user_id: other_user.id }, 
                               authenticity_token: h.form_authenticity_token)
     h.link_to 'Follow', path, remote: true, method: :post
+  end
+
+  def following_text
+    'Following'
   end
 end
