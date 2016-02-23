6fce5450e766b71594eeb40cfd259acd44e9af93
Anonymous <anonymous@example.com>
2016-02-23 12:08:26 +0100

    Renders validation messages, if invalid relationship.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,11 @@
 class RelationshipsController < ApplicationController
   def create
-    relationship = Relationship.new relationship_params
-    relationship.follower = current_user
-    @relationship = Notifier.new(current_user).create! relationship
+    @relationship = Relationship.new relationship_params
+    @relationship.follower = current_user
+    notifier = Notifier.new(current_user)
+    unless notifier.create!(@relationship)
+      render_validation @relationship.errors
+    end
   end
   
   private
