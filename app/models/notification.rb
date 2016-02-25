a03979f0661d4819ebdb061c516f584d5d5832ad
Anonymous <anonymous@example.com>
2016-02-25 11:45:45 +0100

    A Notification can have many users through their user notifications. This is useful for assigning users to a notification.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,7 @@
 class Notification < ApplicationRecord
+
+  has_many :user_notifications
+  has_many :users, through: :user_notifications
+
+  alias_attribute :to_s, :topic
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Notification, :type => :model do
+  subject { FactoryGirl.build :notification }
+
+  describe "associations" do
+    it { is_expected.to have_many(:user_notifications) }
+    it { is_expected.to have_many(:users).through(:user_notifications) }
+  end
+
+  describe "#to_s" do
+    it "should alias #topic" do
+      expect(subject.to_s).to eq(subject.topic)
+    end
+  end
 end
