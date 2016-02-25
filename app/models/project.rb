b375da6b5bfb84cf63bf8dd6f39c565114f93987
Anonymous <anonymous@example.com>
2016-02-25 13:46:33 +0100

    Validates length of projects topic to be between 5 and 200. The strinigfying method 'to_s' is an alias of topic.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ApplicationRecord
   belongs_to :user
-  validates :topic, :user, presence: true
+  
+  validates :user, presence: true
+  validates :topic, presence: true, length: { in: 5..200 }
+
+  scope :by_newest_first, -> { order 'created_at DESC' }
+  
+  alias_attribute :to_s, :topic
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Project, :type => :model do
   describe "validations" do
     it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:topic) }
+    it { is_expected.to validate_length_of(:topic)
+                          .is_at_least(5)
+                          .is_at_most(200) }
     it { is_expected.to validate_presence_of(:user) }
   end
 
